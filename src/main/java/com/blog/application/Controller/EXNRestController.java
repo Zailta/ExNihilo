bc7f4d1d34b46a1261a936b5468e80e4f55eeacc
Zailta <mannanbhat60@gmail.com>
2023-12-27 22:58:13 +0530

    Code changes to make User Deletion an Admin Task

--- src/main/java/com/blog/application/Controller/EXNRestController.java
+++ src/main/java/com/blog/application/Controller/EXNRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -58,6 +59,7 @@ public class EXNRestController {
 		
 	}
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping(value = "/{userID}")
 	public ResponseEntity<EXNAPIResponse> deleteUser(@PathVariable String userID ){
 		 this.exnUserServiceLayer.deleteUser(userID.trim());
